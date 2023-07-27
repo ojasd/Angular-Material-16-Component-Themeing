711c403ad638ba4cd85505012394c46ce389cc9a
ojasd <ojasd@users.noreply.github.com>
2023-07-27 09:18:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Angular-Material-16-Component-Themeing
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/yvwlfn)
---
+++ src/example/slide-toggle-overview-example.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
+/**
+ * @title Basic slide-toggles
+ */
+@Component({
+  selector: 'slide-toggle-overview-example',
+  templateUrl: 'slide-toggle-overview-example.html',
+  standalone: true,
+  imports: [MatSlideToggleModule],
+})
+export class SlideToggleOverviewExample {}
+
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
